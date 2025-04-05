0cbf57d6a26ab7aef2e1ece5e9df8f0209c768b4
SLAVACOM <slavacom121@gmail.com>
2025-04-05 08:45:57 +0300

    finish TaskA

--- src/main/java/com/slavacom/TaskA/TaskA.java
+++ src/main/java/com/slavacom/TaskA/TaskA.java
@@ -1,7 +1,26 @@
 package com.slavacom.TaskA;
 
+import java.util.Scanner;
+
 public class TaskA {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int tRoom = scanner.nextInt();
+        int tCond = scanner.nextInt();
+        scanner.nextLine();
+
+        String mode = scanner.nextLine();
+        System.out.println(taskA(tRoom, tCond, mode));
+    }
+
+    public static int taskA(int tRoom, int tCond, String mode) {
+       return switch (mode){
+           case "freeze" -> Math.min(tRoom, tCond);
+           case "heat" ->  Math.max(tRoom, tCond);
+           case "fan" -> tRoom;
+           case "auto" ->  tCond;
+           default -> -1;
+       };
     }
 }
