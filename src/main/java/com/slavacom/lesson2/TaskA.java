6dd46f827c7a4f02e7a6d43d3c25c8c2803cd459
SLAVACOM <slavacom121@gmail.com>
2025-04-05 12:13:32 +0300

    taskA

--- src/main/java/com/slavacom/lesson2/TaskA.java
+++ src/main/java/com/slavacom/lesson2/TaskA.java
@@ -1,4 +1,18 @@
 package com.slavacom.lesson2;
 
+import java.util.Scanner;
+
 public class TaskA {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] str = scanner.nextLine().split(" ");
+
+        for (int i = 0; i < str.length - 1; i++) {
+            if (str[i + 1].compareTo(str[i]) <= 0) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+    }
 }
