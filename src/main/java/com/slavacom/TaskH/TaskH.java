43baa39465cff960440986d469a1956779169b41
SLAVACOM <slavacom121@gmail.com>
2025-04-05 11:56:21 +0300

    finish TaskH

--- src/main/java/com/slavacom/TaskH/TaskH.java
+++ src/main/java/com/slavacom/TaskH/TaskH.java
@@ -1,42 +1,34 @@
 package com.slavacom.TaskH;
 
+
 import java.util.Scanner;
 
 public class TaskH {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        String pattern = getValidPhoneNumber(sc.nextLine());
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println(pattern.equals(getValidPhoneNumber(sc.nextLine())) ? "YES" : "NO");
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int minN = n + (n - 1) * a;
+        int minM = m + (m - 1) * b;
+
+        int maxN = n + (n + 1) * a;
+        int maxM = m + (m + 1) * b;
+
+        if (maxM < minN || maxN < minM) {
+            System.out.println(-1);
+            return;
         }
+
+        int min = Math.max(minN, minM);
+        int max = Math.min(maxN, maxM);
+
+        System.out.printf("%d %d", min, max);
+
     }
-
-    public static String getValidPhoneNumber(String phoneNumber) {
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < phoneNumber.length(); i++) {
-            char ch = phoneNumber.charAt(i);
-            switch (ch) {
-                case '+':
-                    result.append("8");
-                    i++;
-                case '-':
-                case '(':
-                case ')':
-                    break;
-                default:
-                    result.append(ch);
-            }
-        }
-        switch (result.length()){
-            case 7: result.insert(0, "8");
-            case 8: result.insert(1, "495");
-        }
-        return result.toString();
-    }
-
-
 }
